480da9b25534f09fb591b49968b0b584eab9179c
krystal <krystal.mark@student.moringaschool.com>
2023-06-12 16:44:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+const set = [5, 8, 10, 4, 20, 3]; 
+const max = Math.max(...set); 
+console.log(max); 
+
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4; 
+
+if (mod === 4) {
+  console.log(mod); 
+}
